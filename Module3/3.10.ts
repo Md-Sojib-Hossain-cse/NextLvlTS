5dbc2abb506be90b939d473205b3b8cb1111efd9
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-11-06 23:33:15 +0600

    encapsulation

---
+++ Module3/3.10.ts
@@ -0,0 +1,39 @@
+{
+    //Encapsulation
+    class BankAccount {
+        public readonly id: number;
+        public name: string;
+        protected _balance: number;
+
+        constructor(id: number, name: string, balance: number) {
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+
+        public addDeposit(amount: number) {
+            this._balance = this._balance + amount;
+        }
+
+        private getBalance() {
+            return this._balance;
+        }
+
+        public getHiddenMethod(){
+            return this.getBalance();
+        }
+    }
+
+    class StridentAccount extends BankAccount {
+        test() {
+            this._balance;
+        }
+    }
+
+    const goribManusherAccount = new BankAccount(111, "Mr.Gorib", 100);
+
+    goribManusherAccount.addDeposit(40);
+
+    const myBalance = goribManusherAccount.getBalance();
+    console.log(myBalance);
+}
---
+++ dist/Module3/3.10.js
@@ -0,0 +1,29 @@
+"use strict";
+{
+    //Encapsulation
+    class BankAccount {
+        constructor(id, name, balance) {
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+        addDeposit(amount) {
+            this._balance = this._balance + amount;
+        }
+        getBalance() {
+            return this._balance;
+        }
+        getHiddenMethod() {
+            return this.getBalance();
+        }
+    }
+    class StridentAccount extends BankAccount {
+        test() {
+            this._balance;
+        }
+    }
+    const goribManusherAccount = new BankAccount(111, "Mr.Gorib", 100);
+    goribManusherAccount.addDeposit(40);
+    const myBalance = goribManusherAccount.getBalance();
+    console.log(myBalance);
+}
